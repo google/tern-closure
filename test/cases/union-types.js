5e977aa81f00a6c82ee08f25ec388890bbea6b00
Sean Usick <sean258@gmail.com>
2014-10-09 10:24:59 -0500

    Got union type completion working
    and all tests passing

--- test/cases/union-types.js
+++ test/cases/union-types.js
@@ -5,3 +5,9 @@
 /** @type {(number|string)} */
 var union;
 union; //: (number|string)
+
+
+// observe that some completions are coming from the type number such as
+// Number.prototype.toExponential and others, such as charAt are coming
+// from String.prototype
+union. //+ toString, toFixed, toExponential, charAt, charCodeAt, indexOf, ...
